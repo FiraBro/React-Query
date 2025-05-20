bfc17fe4a2a1d3844843c2a1764ffb5ed7a3a17d
Firagos <jemalfiragos@gmail.com>
2025-05-20 11:01:45 +0300

    the second challenge from chatgpt

--- react-query/src/App.jsx
+++ react-query/src/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 
 export default function App() {
   return (
-    <div className="flex h-screen bg-amber-50 items-center justify-center">
-      <div className="flex flex-col items-center justify-center bg-amber-600 w-3xs h-48">
-        <h1 className="text-amber-50">
-          TanStack
-        </h1>
-        <button className="bg-amber-50 text-amber-300 py-2 px-8 rounded-sm hover:bg-amber-800">Button</button>
+    <div className="flex items-center justify-center">
+      <div className="flex flex-col">
+        <div>
+          <img alt="" src="/ddlogo.jpg" className="w-[100px] rounded-full" />
+          <h2>Firagos Jemal</h2>
+        </div>
+        <div>
+          <p>This the challenge from chatgpt i finally finish it!</p>
+        </div>
       </div>
     </div>
   );
