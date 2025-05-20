2f362736600b0eca1340aedc55ae20e27769a2fd
Firagos <jemalfiragos@gmail.com>
2025-05-20 11:14:56 +0300

    this is the final version of the challenge

--- react-query/src/App.jsx
+++ react-query/src/App.jsx
@@ -2,14 +2,23 @@ import React from "react";
 
 export default function App() {
   return (
-    <div className="flex items-center justify-center">
-      <div className="flex flex-col">
-        <div>
-          <img alt="" src="/ddlogo.jpg" className="w-[100px] rounded-full" />
-          <h2>Firagos Jemal</h2>
+    <div className="flex items-center justify-center h-screen bg-amber-50">
+      <div className="flex flex-col bg-amber-300 text-amber-50 rounded-2xl p-6 max-w-sm shadow-lg">
+        <div className="flex flex-col items-center">
+          <img
+            alt="User avatar"
+            src="/ddlogo.jpg"
+            className="w-[100px] h-[100px] rounded-full"
+          />
+          <h2 className="text-xl font-semibold mt-3">Firagos Jemal</h2>
         </div>
-        <div>
-          <p>This the challenge from chatgpt i finally finish it!</p>
+        <div className="mt-4 text-center">
+          <p className="text-sm mb-4">
+            This is the challenge from ChatGPT. I finally finished it!
+          </p>
+          <button className="px-6 py-3 bg-amber-50 text-amber-300 hover:bg-amber-200 hover:text-amber-600 transition duration-300 rounded-2xl">
+            ChatGPT
+          </button>
         </div>
       </div>
     </div>
