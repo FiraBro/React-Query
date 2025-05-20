b61b9cc4747a22f1748f40a16dc7f47d84183ab3
Hamza Safi <safihamza395@gmail.com>
2025-05-20 07:26:45 +0300

    change this to onther

--- react-query/src/App.jsx
+++ react-query/src/App.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function App() {
   return (
-    <h1>TanStack</h1>
+    <div>
+      <h1>React Query</h1>
+      <p>React Query is a powerful library for managing server state in React applications.</p>
+      <p>It simplifies data fetching, caching, synchronization, and more.</p>
+      <h2>React Query</h2>
+    </div>
   )
 }
